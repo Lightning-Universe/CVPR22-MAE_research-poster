dc49165653e8db0cfbbbdf0842ecc6265538e5a7
Aniket Maurya <theaniketmaurya@gmail.com>
2022-06-24 09:59:02 +0530

    fixes

--- research_app/components/model_demo.py
+++ research_app/components/model_demo.py
@@ -1,8 +1,9 @@
 import logging
+import urllib
 
 import gradio as gr
-from lightning.app.components.serve import ServeGradio
 from PIL import Image
+from lightning.app.components.serve import ServeGradio
 from rich.logging import RichHandler
 
 from research_app.mae_demo import Demo
@@ -11,6 +12,12 @@ FORMAT = "%(message)s"
 logging.basicConfig(level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()])
 
 logger = logging.getLogger(__name__)
+urllib.request.urlretrieve(
+    'https://user-images.githubusercontent.com/11435359/147738734-196fd92f-9260-48d5-ba7e-bf103d29364d.jpg',
+    "resources/fox.jpg")  # fox, from ILSVRC2012_val_00046145
+urllib.request.urlretrieve(
+    'https://user-images.githubusercontent.com/11435359/147743081-0428eecf-89e5-4e07-8da5-a30fd73cc0ba.jpg',
+    "resources/cucumber.jpg")  # cucumber, from ILSVRC2012_val_00047851
 
 
 class ModelDemo(ServeGradio):
@@ -27,7 +34,7 @@ class ModelDemo(ServeGradio):
         gr.outputs.Image(label="reconstructed"),
         gr.outputs.Image(label="reconstructed + visible"),
     ]
-
+    examples = ["resources/fox.jpg", "resources/cucumber.jpg"]
     enable_queue = True
 
     def __init__(self):
